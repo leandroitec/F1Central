e6dc0edaccf6d04708f3d177257f05f1a9b58c09
Ulicrack <u.cabrera@itecriocuarto.org.ar>
2026-06-10 20:38:59 -0300

    correcciones de errores previos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,15 @@
 "use client";
 
 import "./globals.css";
-import Navbar from "@/app/components/Navbar";
-import Footer from "@/app/components/Footer";
-import { FavoritesProvider } from "@/app/favorites/FavoritesContext";
+import { FavoritesProvider } from "@/app/favoritos/FavoritesContext";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className="h-full antialiased">
-      <body className="min-h-full flex flex-col bg-zinc-950 text-white font-sans">
-        
+    <html lang="es">
+      <body className="min-h-screen bg-zinc-950 text-white">
         <FavoritesProvider>
-          <Navbar />
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer />
+          {children}
         </FavoritesProvider>
-
       </body>
     </html>
   );
